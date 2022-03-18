0846f0cc3156067b388ec1a90f27ccb23d5c804e
Yash Sharma <yashsharma2493@gmail.com>
2022-03-18 14:25:19 +0530

    18-03-2022

--- FRONTEND/src/HousDeckWebsiteComponents/DialogsComponents/ServicesDialog.jsx
+++ FRONTEND/src/HousDeckWebsiteComponents/DialogsComponents/ServicesDialog.jsx
@@ -8,7 +8,7 @@ import Typography from '@mui/material/Typography';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import './OnlyForDialog.css';
 import { MiniServices } from '../../constants/data';
-
+import { v4 as uuidV4 } from "uuid"
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
     '& .MuiDialogContent-root': {
@@ -20,15 +20,19 @@ export default function ServicesDialog({ open, setOpen }) {
 
     const fullScreen = useMediaQuery('(max-width:700px)');
     const [price, setPrice] = React.useState(0)
+    const [items, setItems] = React.useState([])
 
     const handleClose = () => {
         setOpen(false)
 
     };
-    function add(data) {
-        if (data) {
-            setPrice((prevPrice) => parseFloat(prevPrice) + parseFloat(data))
-        }
+    function add(service, price) {
+        console.log(service, price);
+        setPrice((prevPrice) => parseFloat(prevPrice) + parseFloat(price))
+        setItems(prevItems => {
+            return [...prevItems, { id: uuidV4(), ServiceChosen: service, PriceForService: price }]
+        })
+        console.log(items)
     }
     return (
         <>
@@ -39,27 +43,40 @@ export default function ServicesDialog({ open, setOpen }) {
                 open={open}
                 maxWidth={false}
             >
-                <Box className='non-scroll' sx={{ height: 'unset', width: '550px', padding: '15px',}}>
+                <Box className='non-scroll' sx={{ height: 'unset', width: '550px', padding: '15px', }}>
                     <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', }}>
                         <Typography sx={{ fontSize: "18px", fontWeight: '700' }}>Select Your Services</Typography>
                         <CloseIcon onClick={handleClose} sx={{ cursor: 'pointer' }} />
                     </Box>
                     <Box>
-                        {MiniServices.map(data =>
+                        {MiniServices.map((data) => {
+                            const service = data.type
+                            const price = data.price
+                            return (
+                                <>
+                                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '100px', width: 'inherit' }}>
+                                        <Typography>{service}</Typography>
+                                        <Typography>&#8377;{price}</Typography>
+                                    </Box>
+                                    <Box sx={{ textAlign: 'end' }}>
+                                        <Button variant='outlined' sx={{ marginLeft: 'auto', marginRight: '0px' }} onClick={() => add(service, price)}>Select</Button>
+                                    </Box>
+                                </>
+                            )
+                        }
+                        )}
+                        <Box sx={{ my: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '100px', }}>
+                            <Typography>Total</Typography>
+                            <Typography>&#8377;{price}</Typography>
+                        </Box>
+                        {items.map(data =>
                             <>
-                                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '100px', width: 'inherit' }}>
-                                    <Typography>{data.type}</Typography>
-                                    <Typography>&#8377;{data.price}</Typography>
-                                </Box>
-                                <Box sx={{ textAlign: 'end' }}>
-                                    <Button variant='outlined' sx={{ marginLeft: 'auto', marginRight: '0px' }} onClick={()=>add(data.price)}>Select</Button>
+                                <Box sx={{ my: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                    <Typography>{data.ServiceChosen}</Typography>
+                                    <Typography>{data.PriceForService}</Typography>
                                 </Box>
                             </>
                         )}
-                        <Box sx={{ my:2,display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '100px',}}>
-                        <Typography>Total</Typography>
-                        <Typography>&#8377;{price}</Typography>
-                        </Box>
                     </Box>
                 </Box>
             </BootstrapDialog>
