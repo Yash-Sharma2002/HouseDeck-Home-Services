56a73d7dcf351223647fd2a0d58b85e5d600550c
Yash Sharma <yashsharma2493@gmail.com>
2022-03-22 20:28:17 +0530

    22-03-2022

--- BACKEND/model/serviceSchema.js
+++ BACKEND/model/serviceSchema.js
@@ -2,22 +2,34 @@ import mongoose from 'mongoose';
 
 const serviceSchema = new mongoose.Schema({
 
-    Service: [{
-        ServiceChoseByUser:  {
+    services: [{
+        ServiceChoseByUser: {
             type: String,
             trim: true,
         },
-        PriceForService:  {
+        PriceForService: {
             type: String,
             trim: true,
         },
-        _id:0
+        _id: 0
     }],
-    TotalPrice: {
+    totalPrice: {
         type: String,
         required: true,
-    }
-    
+    },
+    locationForService: {
+        type: String,
+        required: true,
+    },
+    dateForService: {
+        type: String,
+        // required: true,
+    },
+    timeForService: {
+        type: String,
+        // required: true,
+    },
+
 });
 
 const Service = mongoose.model('service', serviceSchema);
--- FRONTEND/src/HousDeckWebsiteComponents/DialogsComponents/ServicesDialog.jsx
+++ FRONTEND/src/HousDeckWebsiteComponents/DialogsComponents/ServicesDialog.jsx
@@ -13,6 +13,13 @@ import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MyLocationIcon from '@mui/icons-material/MyLocation';
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import TimePicker from '@mui/lab/TimePicker';
+import Stack from '@mui/material/Stack';
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
     '& .MuiDialogContent-root': {
@@ -24,12 +31,22 @@ export default function ServicesDialog({ options, open, setOpen }) {
 
     const fullScreen = useMediaQuery('(max-width:700px)');
     const [price, setPrice] = React.useState(0)
-    const [Service, setService] = React.useState([])
+    const [date, setdate] = React.useState('');
+    const [location, setLocation] = React.useState('');
+    const [time, settime] = React.useState('');
+    const [services, setService] = React.useState([])
+    const [displayForServiceSelectionProcess, setDisplayForServiceSelectionProcess] = React.useState(true)
+    const [displayForStepper, setDisplayForStepper] = React.useState(false)
+    const [displayForAppointment, setDisplayForAppointment] = React.useState(false)
     const [display, setDisplay] = React.useState(false)
 
     const handleClose = () => {
         setOpen(false)
-
+        setdate(null)
+        settime(null)
+        setDisplayForServiceSelectionProcess(true)
+        setDisplayForStepper(false)
+        setDisplayForAppointment(false)
     };
     function add(service, price) {
         if (service && price !== 0) {
@@ -54,8 +71,48 @@ export default function ServicesDialog({ options, open, setOpen }) {
         }
     }
 
+    function getRequiredThings() {
+        setDisplayForServiceSelectionProcess(false)
+        setDisplayForStepper(true)
+        setDisplayForAppointment(false)
+        // var autocomplete = new google.maps.places.Autocomplete((document.getElementById('searchInput')), {
+        //     types: ['geocode'],
+        /*componentRestrictions: {
+         country: "USA"
+        }*/
+        // });
+
+        // google.maps.event.addListener(autocomplete, 'place_changed', function () {
+        //     var near_place = autocomplete.getPlace();
+        // });
+    }
+
+    function locationSubmit() {
+        setDisplayForServiceSelectionProcess(false)
+        setDisplayForStepper(false)
+        setDisplayForAppointment(true)
+    }
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                setLocation(`${position.coords.latitude} ${position.coords.longitude}`)
+            });
+            locationSubmit();
+        } else {
+            console.log("Geolocation is not supported by this browser.")
+        }
+    }
+
     const sendToDatabase = async () => {
-        const items = { Service, TotalPrice: price }
+        const items = {
+            services,
+            totalPrice: price,
+            locationForService: location,
+            dateForService: date,
+            timeForService: time,
+        }
+        console.log(items)
         let response = await serviceSender(items)
         if (response) {
             console.log(response);
@@ -64,7 +121,6 @@ export default function ServicesDialog({ options, open, setOpen }) {
         }
     }
 
-    console.log(Service)
     return (
         <>
             <BootstrapDialog
@@ -74,7 +130,8 @@ export default function ServicesDialog({ options, open, setOpen }) {
                 open={open}
                 maxWidth={false}
             >
-                <Box className='non-scroll' sx={{ height: 'unset', width: '550px', padding: '15px', }}>
+
+                <Box className='non-scroll' sx={{ display: displayForServiceSelectionProcess ? 'block' : 'none', height: '90vh', width: '550px', padding: '15px', }}>
                     <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', }}>
                         <Typography sx={{ fontSize: "18px", fontWeight: '700' }}>Select Your Services</Typography>
                         <CloseIcon onClick={handleClose} sx={{ cursor: 'pointer' }} />
@@ -88,7 +145,7 @@ export default function ServicesDialog({ options, open, setOpen }) {
                                 <>
                                     <Box key={data.type}>
                                         <Box sx={{ textAlign: 'center' }}>
-                                            <img src="../other/cleaning.jpg" style={{ pdding: '10px', marginTop: '10px' }} />
+                                            <img src="../other/cleaning.jpg" alt="" style={{ pdding: '10px', marginTop: '10px' }} />
                                         </Box>
                                         <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', margin: '8px 4px' }}>
                                             <Typography sx={{ fontSize: '18px', fontWeight: '600', fontFamily: 'Fredoka' }}>{service}</Typography>
@@ -112,18 +169,18 @@ export default function ServicesDialog({ options, open, setOpen }) {
                                     <Accordion disableGutters sx={{
                                         boxShadow: 0,
                                     }}>
-                                        <AccordionSummary sx={{padding:0,my: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center', }}>
-                                                <Box sx={{ display: 'flex', alignItems: 'center', }}>
-                                                    <Typography sx={{fontSize: '18px', fontWeight: '600', fontFamily: 'Fredoka' }}>See all items</Typography>
-                                                    <ExpandMoreIcon />
-                                                </Box>
-                                                <Box sx={{ display: 'flex',  alignItems: 'center', }}>
-                                                    <Typography sx={{fontSize: '18px', fontWeight: '600', fontFamily: 'Fredoka' }}>&#8377;{price.toLocaleString()}</Typography>
-                                                    <Button variant='outlined' sx={{ textTransform: 'none' }} onClick={sendToDatabase}>Contnue</Button>
-                                                </Box>
+                                        <AccordionSummary sx={{ padding: 0, my: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center', }}>
+                                            <Box sx={{ display: 'flex', alignItems: 'center', }}>
+                                                <Typography sx={{ fontSize: '18px', fontWeight: '600', fontFamily: 'Fredoka' }}>See all items</Typography>
+                                                <ExpandMoreIcon />
+                                            </Box>
+                                            <Box sx={{ display: 'flex', alignItems: 'center', }}>
+                                                <Typography sx={{ fontSize: '18px', fontWeight: '600', fontFamily: 'Fredoka' }}>&#8377;{price.toLocaleString()}</Typography>
+                                                <Button variant='outlined' sx={{ textTransform: 'none' }} onClick={getRequiredThings}>Contnue</Button>
+                                            </Box>
                                         </AccordionSummary>
                                         <AccordionDetails>
-                                            {Service.map(data =>
+                                            {services.map(data =>
                                                 <>
                                                     <Box sx={{ my: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                                                         <Typography>{data.ServiceChoseByUser}</Typography>
@@ -142,6 +199,80 @@ export default function ServicesDialog({ options, open, setOpen }) {
 
                     </Box>
                 </Box>
+
+                {/* ---------------------Stepper code is here -------------- */}
+
+                <Box sx={{ display: displayForStepper ? 'block' : 'none', height: '90vh', width: '550px', padding: '15px', }}>
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', }}>
+                        <Typography sx={{ fontSize: "18px", fontWeight: '700' }}>Select Your Services</Typography>
+                        <CloseIcon onClick={handleClose} sx={{ cursor: 'pointer' }} />
+                    </Box>
+
+                    <Box>
+                        <input
+                            id='searchInput'
+                            placeholder='Enter your location'
+                            type='text'
+                            // onChange={e => handleNumChange(e.target)}
+                            style={{
+                                border: '1px solid #e5e5e5',
+                                userSelect: 'none',
+                                width: '97%',
+                                height: '40px',
+                                fontSize: '14px',
+                                marginTop: '30px',
+                                paddingLeft: '10px'
+                            }} />
+                    </Box>
+
+                    <Box sx={{ display: 'flex', justifyContent: 'start', alignItems: 'center', color: 'rgb(25, 118, 210)', my: 4 }} onClick={getLocation}>
+                        <MyLocationIcon />
+                        <Typography sx={{ fontSize: '16px', ml: 1 }} >current location</Typography>
+                    </Box>
+                    <Box sx={{ textAlign: 'end', mt: 52 }}>
+                        <Button sx={{ fontSize: '16px', marginLeft: 'auto', marginRight: '0px', textTransform: 'none' }} variant='outlined' onClick={locationSubmit}>Continue</Button>
+                    </Box>
+                </Box>
+
+                <Box sx={{ display: displayForAppointment ? 'block' : 'none', height: '90vh', width: '550px', padding: '15px', }}>
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', }}>
+                        <Typography sx={{ fontSize: "18px", fontWeight: '700' }}>Select Date and Time</Typography>
+                        <CloseIcon onClick={handleClose} sx={{ cursor: 'pointer' }} />
+                    </Box>
+
+                    <Box>
+                        <LocalizationProvider dateAdapter={AdapterDateFns}>
+                            <Stack spacing={1} sx={{ width: '300px' }}>
+                                <DatePicker
+                                    label="Enter date"
+                                    value={date}
+                                    disablePast
+                                    calenders={1}
+                                    onChange={(newdate) => {
+                                        setdate(newdate.slice(0,15));
+                                    }}
+                                    renderInput={(params) => <TextField sx={{ my: 4 }} {...params} />}
+                                />
+
+                                <TimePicker
+                                    renderInput={(params) => <TextField {...params} />}
+                                    value={time}
+                                    label="Enter Time"
+                                    onChange={(newtime) => {
+                                        settime(newtime.slice(16,25));
+                                    }}
+                                    minTime={new Date(0, 0, 0, 8)}
+                                    maxTime={new Date(0, 0, 0, 18, 45)}
+                                />
+                            </Stack>
+                        </LocalizationProvider>
+                    </Box>
+
+                    <Box sx={{ textAlign: 'end', mt: 45 }}>
+                        <Button sx={{ fontSize: '16px', marginLeft: 'auto', marginRight: '0px', textTransform: 'none' }} variant='outlined' onClick={sendToDatabase}>Continue</Button>
+                    </Box>
+                </Box>
+
             </BootstrapDialog>
         </>
     )
