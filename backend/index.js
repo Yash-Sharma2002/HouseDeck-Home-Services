cfe3905b28ab3db92d3a43cccb05a9c61c789f63
Yash Sharma <yashsharma2493@gmail.com>
2022-07-13 18:07:54 +0530

    production

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 dotenv.config({ path: './config.env' })
 
 const DB_URL = process.env.DB_URL
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 const app = express();
 
 
@@ -23,11 +23,11 @@ app.use(cors());
 app.use('/', router);
 app.use(express.json())
 
-if (process.env.NODE_ENV == "production") {
+// if (process.env.NODE_ENV == "production") {
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname,'FRONTEND','build','index.html'));
 })
-}
+// }
 
 Connection(DB_URL)
 
